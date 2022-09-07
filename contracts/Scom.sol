f5fdfb68b5d124fcabb0fa299be4bd9fcadfd156
nigellai2020 <65589812+nigellai2020@users.noreply.github.com>
2022-09-07 17:44:15 +0800

    Add Scom.sol

---
+++ contracts/Scom.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+
+contract Scom is ERC20Capped {
+    address public immutable minter;
+
+    constructor(address _minter, address initSupplyTo, uint initSupply, uint256 totalSupply) ERC20("SCOM", "SCOM") ERC20Capped(totalSupply) public {
+        minter = _minter;
+        _mint(initSupplyTo, initSupply);
+    }
+
+    function mint(address account, uint256 amount) external {
+        require(msg.sender == minter, "Not from minter");
+        _mint(account, amount);
+    }
+}
