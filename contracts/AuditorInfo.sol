6f7c031d3ca247b0d75b992fe09b075fde6dd1e1
nigellai2020 <65589812+nigellai2020@users.noreply.github.com>
2022-09-08 12:06:17 +0800

    Update AuditorInfo.sol: Add cooldown period

--- contracts/AuditorInfo.sol
+++ contracts/AuditorInfo.sol
@@ -14,32 +14,59 @@ contract AuditorInfo is Authorization, ReentrancyGuard {
         address auditor;
         AuditorStatus status;
     }
+    struct WithdrawRequest {
+        uint256 amount;
+        uint256 releaseTime;
+    }
 
     IERC20 public immutable token;
     uint256 public auditorIdCount;
+    uint256 public cooldownPeriod;
+    uint256 public constant MAX_COOLDOWN_PERIOD = 604800; // 1 week
 
     mapping(uint256 => AuditorData) public auditorsData; //auditorsData[auditorId] = AuditorData
     mapping(address => uint256) public auditorIds; //auditorIds[address] = id
     mapping(uint256 => uint256) public auditorBalance; //auditorBalance[auditorId] = amount
+    mapping(uint256 => WithdrawRequest) public pendingWithdrawal; //pendingWithdrawal[auditorId] = WithdrawRequest
 
     event AddAuditor(address indexed auditor);
-    event RemoveAuditor(address indexed auditor);
+    event DisableAuditor(address indexed auditor);
+    event SetCooldownPeriod(uint256 cooldownPeriod);
     event StakeBond(address indexed sender, uint256 amount, uint256 newBalance);
-    event UnstakeBond(address indexed sender, uint256 amount, uint256 newBalance);
+    event UnstakeBondRequest(address indexed sender, uint256 amount, uint256 newBalance);
+    event WithdrawBond(address indexed sender, uint256 amount);
 
-    constructor(IERC20 _token) {
+    constructor(IERC20 _token, uint256 _cooldownPeriod) {
         token = _token;
+        cooldownPeriod = _cooldownPeriod;
     }
 
-    modifier onlyAuditor {
+    modifier onlyActiveAuditor {
         uint256 auditorId = auditorIds[msg.sender];
-        require(auditorId > 0 && auditorsData[auditorId].status == AuditorStatus.Active, "not from auditor");
+        require(auditorId > 0 && auditorsData[auditorId].status == AuditorStatus.Active, "not from active auditor");
         _;
     }
 
-    function isAuditor(address account) external returns (bool) {
+    function isActiveAuditor(address account) external view returns (bool) {
         uint256 auditorId = auditorIds[account];
         return auditorId > 0 && auditorsData[auditorId].status == AuditorStatus.Active;
+    }
+
+    function getAuditors(uint256 auditorIdStart, uint256 length) 
+        external 
+        view
+        returns (
+            AuditorData[] memory auditors
+        ) 
+    {
+        if (auditorIdStart + length > auditorIdCount + 1) {
+            length = auditorIdCount - auditorIdStart + 1;
+        }
+        auditors = new AuditorData[](length);
+        for (uint256 i; i < length; i++) {
+            auditors[i] = auditorsData[auditorIdStart];
+            auditorIdStart++;
+        }
     }
 
     function addAuditor(address auditor) external onlyOwner {
@@ -60,15 +87,22 @@ contract AuditorInfo is Authorization, ReentrancyGuard {
         emit AddAuditor(auditor);
     }
 
-    function removeAuditor(address auditor) external onlyOwner {
+    function disableAuditor(address auditor) external onlyOwner {
         uint256 auditorId = auditorIds[auditor];
+        require(auditorId > 0, "auditor not exist");
         AuditorData storage auditorData = auditorsData[auditorId];
-        require(auditorId > 0 && auditorData.status == AuditorStatus.Active, "auditor not exist");
+        require(auditorData.status == AuditorStatus.Active, "auditor already disabled");
         auditorData.status = AuditorStatus.Inactive;
-        emit RemoveAuditor(auditor);
+        emit DisableAuditor(auditor);
     }
 
-    function stakeBond(uint256 amount) external onlyAuditor nonReentrant {
+    function setCooldownPeriod(uint256 _cooldownPeriod) external onlyOwner {
+        require(_cooldownPeriod <= MAX_COOLDOWN_PERIOD, "Max cooldown period > 1 week!");
+        cooldownPeriod = _cooldownPeriod;
+        emit SetCooldownPeriod(cooldownPeriod);
+    }
+
+    function stakeBond(uint256 amount) external onlyActiveAuditor nonReentrant {
         require(amount > 0, "amount = 0");
         amount = _transferTokenFrom(amount);
         uint256 auditorId = auditorIds[msg.sender];
@@ -77,13 +111,31 @@ contract AuditorInfo is Authorization, ReentrancyGuard {
         emit StakeBond(msg.sender, amount, newBalance);
     }
 
-    function unstakeBond(uint256 amount) external onlyAuditor nonReentrant {
+    function unstakeBondRequest(uint256 amount) external onlyActiveAuditor nonReentrant {
         require(amount > 0, "amount = 0");
         uint256 auditorId = auditorIds[msg.sender];
         uint256 newBalance = auditorBalance[auditorId] - amount;
         auditorBalance[auditorId] = newBalance;
+        if (cooldownPeriod == 0) {
+            token.safeTransfer(msg.sender, amount);
+            emit WithdrawBond(msg.sender, amount);
+        }
+        else {
+            WithdrawRequest storage request = pendingWithdrawal[auditorId];
+            request.amount += amount;
+            request.releaseTime = block.timestamp + cooldownPeriod;
+        }
+        emit UnstakeBondRequest(msg.sender, amount, newBalance);
+    }
+
+    function withdrawBond() external onlyActiveAuditor nonReentrant {
+        uint256 auditorId = auditorIds[msg.sender];
+        WithdrawRequest storage withdrawRequest = pendingWithdrawal[auditorId];
+        require(block.timestamp >= withdrawRequest.releaseTime, "please wait");
+        uint256 amount = withdrawRequest.amount;
+        delete pendingWithdrawal[auditorId];
         token.safeTransfer(msg.sender, amount);
-        emit UnstakeBond(msg.sender, amount, newBalance);
+        emit WithdrawBond(msg.sender, amount);
     }
 
     function _transferTokenFrom(uint amount) internal returns (uint256 balance) {
