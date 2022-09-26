469c433e51320139fbda7f2345e7188eeadba5fa
nigellai2020 <65589812+nigellai2020@users.noreply.github.com>
2022-09-26 16:23:18 +0800

    Update contracts: remove constructor visibility in Scom.sol

--- contracts/Scom.sol
+++ contracts/Scom.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 contract Scom is ERC20Capped {
     address public immutable minter;
 
-    constructor(address _minter, address initSupplyTo, uint initSupply, uint256 totalSupply) ERC20("SCOM", "SCOM") ERC20Capped(totalSupply) public {
+    constructor(address _minter, address initSupplyTo, uint initSupply, uint256 totalSupply) ERC20("SCOM", "SCOM") ERC20Capped(totalSupply) {
         minter = _minter;
         _mint(initSupplyTo, initSupply);
     }
